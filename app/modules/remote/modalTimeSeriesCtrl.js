6ed9ee858fc422690dd6ce5a33d2a88090f0e1a7
jiaping.feng <jiaping.feng@banxiniot.com>
2018-04-28 09:47:15 +0800

    cvsUrl

--- app/modules/remote/modalTimeSeriesCtrl.js
+++ app/modules/remote/modalTimeSeriesCtrl.js
@@ -191,35 +191,8 @@
 			}
 		};
 
-		/* downloadCsv button
-		*/
-		vm.downloadCsv = function(){
-			var params = {},
-				res_date = getStartEndDate(),
-				ids = getId().join(",");
-
-			var current_date = new Date();
-			var gmtHours = -1 * current_date.getTimezoneOffset()/60;
-			var current_timezone = "GMT+" + gmtHours;
-
-			params = {
-				datapoints: ids,
-				resolution: vm.resolutions.model,
-				from: res_date.start,
-				to: res_date.end,
-				timezone: current_timezone
-			};
-
-			if(angular.isDefined(params) && params.datapoints!==""){
-				apiService.timeSeriesRangeCsvApi(params).then(function(response){
-					console.log(response);
-					$window.open("data:text/csv;charset=utf-8," + encodeURIComponent(response.data));
-				});
-			}else{
-				dialogService.alert(null,{content:$translate.instant('site_common_something_wrong')});
-			}
-		};
-
+		vm.csvUrl = "https://smartwater.banxiniot.com/api/tsda/query/5a6eb4e70b272a1f64fa26ba-pressure/1n/1523327700000/1523332740000/csv/GMT+8?token=617a1a81-0655-444e-a8cb-ea9daf0e406d";
+		
 		/* filter button
 		*/
 		vm.getTimeSeriesFilter = function(){
